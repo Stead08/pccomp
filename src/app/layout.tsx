f22a7f237f8f8d493eb990750f51fd9931bffe60
Stead08 <inomatama08@gmail.com>
2023-06-01 20:54:42 +0900

    fixed metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,20 @@ const inter = Inter({subsets: ["latin"]});
 export const metadata = {
   title: "自作PC構成 by Stead08",
   description: "自作PCの構成表を楽天市場の商品から手軽に作成できます。",
+  openGraph: {
+    title: "自作PC構成 by Stead08",
+    description: "自作PCの構成表を楽天市場の商品から手軽に作成できます。",
+    type: "website",
+    url: "https://pccomp.vercel.app/",
+    locale: 'ja_JP',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: "自作PC構成 by Stead08",
+    description: "自作PCの構成表を楽天市場の商品から手軽に作成できます。",
+    site: '@Stead08_',
+    creator: '@Stead08_',
+  },
 };
 
 export default function RootLayout({
